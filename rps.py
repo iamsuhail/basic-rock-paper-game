f015d93c901d3c9fba132838465f9622f9f72cee
suahil <s.muhammadsuhail123@gmail.com>
2021-12-03 10:14:13 +0530

    suhail

--- rps.py
+++ rps.py
@@ -19,6 +19,4 @@ def is_win(player,opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
     
-    
-
 print(play())
